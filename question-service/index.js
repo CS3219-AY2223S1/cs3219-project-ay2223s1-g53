ca46b4f5767c66d65fc3b0a19dd018a6e79ba4cd
iztanpy <61683649+iztanpy@users.noreply.github.com>
2022-11-03 15:25:04 +0800

    Restructure pages in frontend
    Add app bar w password change
    Restructure code chat page

--- question-service/index.js
+++ question-service/index.js
@@ -3,18 +3,23 @@ const questionOrm = require("./question-orm.js");
 const app = express();
 const port = 3005;
 app.use(express.json());
+const cors = require("cors");
+
+app.use(cors()); // config cors so that front-end can use
+app.options("*", cors());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+
 app.get("/", async (req, res) => {
   const difficulty = req.query.difficulty;
   const result = await questionOrm.getRandomQuestionByDifficulty(difficulty);
   res.json({
-    "message":"success",
-    "data":result
-});
+    message: "success",
+    data: result,
+  });
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
